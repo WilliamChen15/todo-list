0bbd3973f25857a2fec947002ad770fe27fd0dbe
WilliamChen15 <turelight13@gmail.com>
2022-12-10 23:26:42 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars');
 // 引用 body-parser
 const bodyParser = require('body-parser')
-
-// // 引用 body-parser
-// const bodyParser = require('body-parser')
+// 載入 method-override
+const methodOverride = require('method-override')
 
 const Todo = require('./models/todo') // 載入 Todo model
 
@@ -35,7 +34,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
 // 設定首頁路由
 // Todo 首頁
 app.get('/', (req, res) => {
