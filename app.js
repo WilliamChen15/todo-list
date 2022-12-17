3f0bd9efb40f9192b7c6ba94419be7ade49fb773
WilliamChen15 <turelight13@gmail.com>
2022-12-17 12:03:45 +0800

    feat: add bootstrap & modify main.hbs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
